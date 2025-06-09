1f7473b2d8c13c02f426177e17591def5417158d
Yannik Braendle <yannik.braendle@t-online.de>
2025-06-10 00:34:40 +0200

    shader extraction scene

---
+++ Magtong/scenes/tools/shader_image_extractor.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var subvp: SubViewport = get_node("SubViewport")
+
+func _ready():
+	await get_tree().create_timer(0.5).timeout
+	var tex = subvp.get_texture()
+	await RenderingServer.frame_post_draw
+	var img = tex.get_image()
+	var image_tex = ImageTexture.create_from_image(img)
+	var path = "res://cool_img.tres"
+
+	ResourceSaver.save(image_tex, path)
+	img.save_png("res://cool_png.png")
+	# quit
+	get_tree().quit()
+	pass
